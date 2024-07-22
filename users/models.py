aac309f26eaa7614054964cf7e1718009952de3b
shehata <msb330033@gmail.com>
2024-07-22 13:21:35 +0300

    User ORM edit, serializers

--- users/models.py
+++ users/models.py
@@ -12,52 +12,59 @@ class Gender(models.TextChoices):
     FEMALE = "F", "Female"
 
 
-class User(models.Model):
+class Identity(models.TextChoices):
+    PATIENT = "P", "Patient"
+    Nurse = "D", "Nurse"
+    ADMIN = "A", "Admin"
+
+
+# remember to create class for user payment methods
+class User(AbstractUser):
     """
     User override model for authentication.
     """
 
     id = models.AutoField(primary_key=True)
     national_id = models.CharField(max_length=55, unique=True)
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=20)
+    identity = models.CharField(max_length=8, choices=Identity)
     gender = models.CharField(max_length=8, choices=Gender)
+    phone_number = models.CharField(max_length=20)
     bio = models.TextField(blank=True)
     nationality = models.CharField(max_length=100)
     location = models.CharField(max_length=55)
     city = models.CharField(max_length=55)
     country = models.CharField(max_length=55)
     date_of_birth = models.DateField()
-    date_joined = models.DateTimeField(default=timezone.now)
+
+    is_staff = None
+    is_active = None
+    last_login = None
     
     # django depend on username in authentication process but
     # we want to depend on email in authentication process
     # because email is unique field
     # so we have to manually set username field to email
-    username = None
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
 
     class Meta:
         abstract = True
 
 
+# remember to create list of diseases
 class Patient(User):
-    profile_image = models.ImageField(upload_to="media/profile_images/", blank=True, null=True)
+    profile_image = models.ImageField(upload_to="profile_images/", blank=True, null=True)
     chronic_diseases = models.CharField(max_length=255, blank=True, null=True)
-    medical_report = models.FileField(upload_to='media/reports/', blank=True, null=True)
+    medical_report = models.FileField(upload_to='reports/', blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
-class Doctor(User):
-    profile_image = models.ImageField(upload_to="media/profile_images/")
+class Nurse(User):
+    profile_image = models.ImageField(upload_to="profile_images/")
     specialization = models.CharField(max_length=100)
-    certificates = models.FileField(upload_to='media/certificates/', blank=True, null=True)
-    medical_accreditations = models.CharField(max_length=255, blank=True)
+    certificates = models.FileField(upload_to='certificates/')
+    medical_accreditations = models.FileField(upload_to='accreditations/')
     available_working_hours = models.CharField(max_length=255)
 
     def __str__(self):
@@ -65,24 +72,27 @@ class Doctor(User):
 
 
 class Admin(User):
-    profile_image = models.ImageField(upload_to="media/profile_images/")
+    profile_image = models.ImageField(upload_to="profile_images/")
 
     def __str__(self):
         return self.name
 
 
+class AbstractSession(models.Model):
+    pass
+
 class Session(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='sessions')
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='sessions')
+    nurse = models.ForeignKey(Nurse, on_delete=models.CASCADE, related_name='sessions')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     remaining_price = models.DecimalField(max_digits=10, decimal_places=2)
     total_sessions = models.PositiveIntegerField()
     remaining_sessions = models.PositiveIntegerField()
-    # prev_session = models.OneToOneField("self", on_delete=models.CASCADE, related_name='prev', null=True)
-    # next_session = models.OneToOneField("self", on_delete=models.CASCADE, related_name='next', null=True)
+    prev_session = models.OneToOneField("self", on_delete=models.CASCADE, related_name='prev', null=True)
+    next_session = models.OneToOneField("self", on_delete=models.CASCADE, related_name='next', null=True)
     place = models.CharField(max_length=100)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
     def __str__(self):
-        return f"Session between {self.patient.name} and {self.doctor.name}"
+        return f"Session between {self.patient.name} and {self.nurse.name}"
--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Patient, Nurse, Admin, Session
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,16 +11,21 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = [
             "id",
-            "identity",
-            "name",
+            "national_id",
+            "username",
+            "first_name",
+            "last_name",
             "email",
             "password",
-            "profile_image",
-            "bio",
-            "birth_date",
+            "identity",
             "gender",
-            "phone",
-            "address",
+            "phone_number",
+            "bio",
+            "nationality",
+            "location",
+            "city",
+            "country",
+            "date_of_birth",
         ]
         extra_kwargs = {
             "password": {"write_only": True},
@@ -36,3 +41,68 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
         instance.save()
         return instance
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    """
+    Serializer for patient model
+    """
+    
+    class Meta:
+        model = Patient
+        fields = [
+            "profile_image",
+            "chronic_diseases",
+            "medical_report",
+        ]
+
+
+class NurseSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Nurse model
+    """
+    
+    class Meta:
+        model = Nurse
+        fields = [
+            "profile_image",
+            "specialization",
+            "certificates",
+            "medical_accreditations",
+            "available_working_hours"
+        ]
+
+
+class AdminSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Admin model
+    """
+    
+    class Meta:
+        model = Admin
+        fields = ["profile_image"]
+
+
+class SessionSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Session model.
+    """
+
+    class Meta:
+        model = Session
+        fields = [
+            "patient",
+            "nurse",
+            "price",
+            "remaining_price",
+            "total_sessions",
+            "remaining_sessions",
+            "prev_session",
+            "next_session",
+            "place",
+            "start_time",
+            "end_time",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
